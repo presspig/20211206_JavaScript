75b3f95561056ef6f299d149e6bbb2a94b796a0b
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-08 12:05:16 +0900

    .

--- JavaScript/Chap05_Function/A04Callback.js
+++ JavaScript/Chap05_Function/A04Callback.js
@@ -8,25 +8,102 @@ console.log('---------------- [182] 중첩함수, 콜백함수 -----------------
     4. 동적으로 프로퍼티 생성 가능
 */
 
+var ary = [ function() { return 10} ];
+console.log( ary[0]() )
+console.log('');
+
 // 중첩함수
 function outer() {
     var x = 10;
 
+    function inner() {
+        console.log('inner=> ', x);
+    }
+
+    inner();
 }
 outer();
 console.log('');
 
 
 function outer2(x) {
-    console.log('outer 함수 호출', x);
-
+    console.log('outer 함수 호출');
+    // console.log(x);
+    x();
 }
 
-var inner = outer2(10);
+function inner() {
+    console.log('Inner');
+}
 
-
+var inner = outer2(inner);
 
 
 // 함수의 인자나 리턴 값이 될 수 있다.
+function outer3(x) {
+    console.log('outer 함수 호출', x);
 
+    return function() {
+        console.log('Return 함수', x)
+    }
+}
+
+var inner3 = outer3(100);
+console.log(inner3);
+inner3();
+
+// callback 함수 사용 예제
+function onAdd(x, y) {
+    var result = x + y;
+    return result + 100;
+};
+
+function onMin(x, y) {
+    var result = x + y;
+    return result - 100;
+};
+
+console.log(onAdd(10, 20));
+console.log(onMin(10, 30));
+console.log('');
+
+
+function onMulti(x, y, callback) {
+    var result = x + y;
+    callback(result);
+}
+
+onMulti(10, 20, function(data) {
+    console.log(data * 10);
+});
+
+onMulti(10, 20, function(data) {
+    console.log(data / 10);
+});
+
+
+console.log('')
+
+
+
+// 시간이 걸리는 작업
+var value = 0;
+function getData(url, cb) {
+    console.log('1 Start', url)
+
+    setTimeout( function(){
+        console.log('setTimeout');
+        value = 100;
+        
+        cb(value);
+    }, 1000);
+
+    console.log('END')
+};
+
+getData('http://localhost', function(data) {
+    value = data;
+    console.log(value)
+});
+console.log(value);
 
--- JavaScript/Chap05_Function/A06Scope.js
+++ JavaScript/Chap05_Function/A06Scope.js
@@ -1,37 +1,74 @@
 
 console.log('---------------- [189] Scope -----------------');
 
+// var 변수도 함수의 {} 내부에서는 범위를 갖는다.
 var x = 'Global';
 
-function out(){
-    var x = 'Local';
-    console.log('Function IN: ', x);
+function out() {
+    var x = 'Local';            // x는 지역변수. 함수 {} 내부에서만 참조 가능한 변수
+    var z = '밖에서 참조 안됨';
+    console.log('Function IN: ', x, z);
 }
-
+out();
+// console.log(z);
 console.log('Function OUT: ', x);
-
+console.log('');
 
 // 197 Page
+
+// arguments 객체 생성
+// scope chian 설정
+// 내부 변수 정의
+// this 값 결정
+// 실행
+
 var x = 'Global X';
 var y = 'Global Y';
 
 function outer() {
     var z = 'Outer Local z';
 
+    console.log('outer=> ', x);
+    console.log('outer=> ', y);
+    console.log('outer=> ', z);
+
+    function inner() {
+        var z = 'Inner Local z';
+
+        console.log('inner=> ', x);
+        console.log('inner=> ', y);
+        console.log('inner=> ', z);
+    }
+
+    inner();
 };
+
+// inner();
+console.dir(outer);
 
 outer();
 console.log('');
 
 
+var x = 'Global X';
 
 function foo() {
     var x = 10;
-    bar();
+    bar();          // 평가될때 상위 Scope가 window로 지정된 상태라 window의 x를 출력.
 };
 
+// 이 함수가 평가될때 scope chain은 window 밑에 기술된 함수라 상위 스코프가 window
 function bar() {
     console.log(x);
 };
 
-bar();
+// bar();
+foo();
+
+
+function outer3() {
+
+    function inner() {
+        
+    }
+}
