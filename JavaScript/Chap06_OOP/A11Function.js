494ec3c5c2934e896006c2fbd6cba8c6824ab6be
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-09 10:59:44 +0900

    .

--- JavaScript/Chap06_OOP/A11Function.js
+++ JavaScript/Chap06_OOP/A11Function.js
@@ -1,23 +1,48 @@
 
 console.log('---------------- [388] Closure -----------------');
 
-var A = 10;
-var alert = 100;
-
 var x = 10;
 
 function one() {
     var x = 'one';
-
-}
-one();
-
-{
-    let x = 10
+    two();
 }
 
 function two() {
     console.log(x);
 };
+one();
 
+function three() {
+    var x = 'three';
+    var y = 'three y';
+
+    function inner() {
+        console.log(x, y);
+    }
+    inner();
+}
+three();
+console.log(x);             // window의 10
+// console.log(y);          // Error
+
+
+// closure
+function four() {
+    var x = 1;
+
+    function inner() {
+        // console.log(x);
+        x = x + 100;
+        return x;
+    }
+    // var result = inner();
+    return inner;
+}
+var inner = four();             // 함수 리터럴이 전달된다
+var result = inner();
+console.log(result)
+
+var inner2 = four()();
+console.log(inner2);
 
--- JavaScript/Chap06_OOP/A12Closure.js
+++ JavaScript/Chap06_OOP/A12Closure.js
@@ -4,5 +4,58 @@ console.log('---------------- [401] Closure -----------------');
 let num = 0;
 
 const incOne = function() {
-    
+    return ++num;
 };
+console.log(incOne());
+console.log(incOne());
+console.log(incOne());
+
+num = 100;
+console.log(incOne());
+console.log('');
+
+
+const incTwo = function() {
+    let cnt = 0;
+    return ++cnt;
+};
+console.log(incTwo());
+console.log(incTwo());
+console.log(incTwo());
+console.log('');
+
+
+var inc = function() {
+    var cnt = 0;
+
+    return cnt++;
+}
+inc();
+// console.log(cnt++)
+
+// closure
+var incThree = function() {
+    let cnt = 0;
+
+    function inner() {
+        return ++cnt
+    }
+
+    return inner;
+}
+
+let inner = incThree();
+// 결론적으로는 incThree()를 호출하면 이렇게 리턴되서 정의된 것과 동일. 받은 값이 함수 리터럴(문자)다.
+// let inner = function() { return ++cnt };
+
+// 리턴 받은 함수 리터럴 값을 ()를 이용해 함수로써 실행한다.
+// 이 함수는 내부 변수 cnt를 참조하여 값을 1 증가한다.
+console.log(inner());   
+console.log(inner());       // inner 함수가 cnt 값을 참조하고 있으므로 cnt 변수가 사라지지 않고 증가된 값에 1을 더해 2가 출력된다.
+console.log(inner());
+
+// 별도의 객체가 생성됨
+const inner2 = incThree();
+console.log(inner2());
+console.log(inner2());
+console.log(inner2());
