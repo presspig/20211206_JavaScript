54924a65f7ed19ae45dbd3b8acb819044bc7644b
SHINSUNGJIN <pxp13716@gmail.com>
2021-12-09 11:03:30 +0900

    .

--- JavaScript/Chap06_OOP/A12Closure.js
+++ JavaScript/Chap06_OOP/A12Closure.js
@@ -54,7 +54,11 @@ console.log(inner());
 console.log(inner());       // inner 함수가 cnt 값을 참조하고 있으므로 cnt 변수가 사라지지 않고 증가된 값에 1을 더해 2가 출력된다.
 console.log(inner());
 
-// 별도의 객체가 생성됨
+// 실행 컨텍스트에 다시 incThree가 실행되면서 별도의 객체가 생성됨
+// 따라서 위의 inner와는 다른 메모리 번지주에 생성된 inner 함수 리터럴을 받아서 쳐다보고 있다.
+// 따라서 위의 inner 변수와는 별도로 동작하게 된다.
+// 여기서도 이렇게 별도로 받음
+// let inner = function() { return ++cnt };
 const inner2 = incThree();
 console.log(inner2());
 console.log(inner2());
